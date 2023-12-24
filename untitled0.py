4b4128925ad5f7bcafb96eab731ac3e9d26632cc
surya chanda <83336212+suryavamsiq@users.noreply.github.com>
2023-12-24 19:22:06 +0530

    untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -8,14 +8,15 @@ import pickle
 import numpy as np
 import streamlit as st
 
-try:
-    loaded_model = pickle.load(open('1trainedmodel.sav', 'rb'))
-except Exception as e:
-    st.error(f"Error loading the model: {e}")
-    # Add more detailed information about the exception if needed
-    # For example, print(traceback.format_exc()) to print the full traceback
+def load_model():
+    try:
+        loaded_model = pickle.load(open('1trainedmodel.sav', 'rb'))
+        return loaded_model
+    except Exception as e:
+        st.error(f"Error loading the model: {e}")
+        return None
 
-def prediction(input_data):
+def prediction(loaded_model, input_data):
     try:
         # Convert input_data to float
         newValues1 = np.array(input_data, dtype=float)
@@ -26,7 +27,6 @@ def prediction(input_data):
         return ('The recommended model for your data is', prediction_result)
     except Exception as e:
         return f"Error during prediction: {e}"
-      # or handle the error in an appropriate way
 
 def main():
     st.title("CROP RECOMMENDATION SYSTEM WEB APP")
@@ -39,9 +39,11 @@ def main():
     ph = st.text_input('Enter value of pH')
     rainfall = st.text_input('Enter value of rainfall')
     
+    loaded_model = load_model()
+    
     output = ''
-    if st.button('Recommend Crop'):
-        output = prediction([nitrogen, phosphorus, potassium, temp, humidity, ph, rainfall])
+    if loaded_model is not None and st.button('Recommend Crop'):
+        output = prediction(loaded_model, [nitrogen, phosphorus, potassium, temp, humidity, ph, rainfall])
     
     st.success(output) 
 
