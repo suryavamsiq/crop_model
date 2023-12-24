7cbc964a6d0fb4552c4851918c64349f7e505dc0
surya chanda <83336212+suryavamsiq@users.noreply.github.com>
2023-12-24 19:34:31 +0530

    untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,50 +1,40 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Dec 24 10:56:34 2023
+
 @author: c surya vamsi
 """
 
 import pickle
 import numpy as np
+loaded_model=pickle.load(open('C:/Users/c surya vamsi/1trainedmodel.sav','rb'))
 import streamlit as st
 
-def load_model():
-    try:
-        loaded_model = pickle.load(open('1trainedmodel.sav', 'rb'))
-        return loaded_model
-    except Exception as e:
-        st.error(f"Error loading the model: {e}")
-        return None
-
-def prediction(loaded_model, input_data):
-    try:
-        # Convert input_data to float
-        newValues1 = np.array(input_data, dtype=float)
-        newValues1 = newValues1.reshape(1, -1)
-        
-        prediction_result = loaded_model.predict(newValues1)
-        print("Prediction result:", prediction_result)
-        return ('The recommended model for your data is', prediction_result)
-    except Exception as e:
-        return f"Error during prediction: {e}"
-
+def prediction(input_data):
+    newValues1=np.array(input_data)
+    newValues1=newValues1.reshape(1,-1)
+    return ('The recommended model for your data is',loaded_model.predict(newValues1))
+    
 def main():
     st.title("CROP RECOMMENDATION SYSTEM WEB APP")
+    #N	P	K	temperature	humidity	ph	rainfall	label
+    N=st.text_input('enter value of N')
+    P=st.text_input('enter value of P')
+    K=st.text_input('enter value of k')
+    temp=st.text_input('enter value of temprature')
+    humidity=st.text_input('enter value of humidity')
+    ph=st.text_input('enter value of ph')
+    rainfall=st.text_input('enter value of rainfall')
     
-    nitrogen = st.text_input('Enter value of N')
-    phosphorus = st.text_input('Enter value of P')
-    potassium = st.text_input('Enter value of K')
-    temp = st.text_input('Enter value of temperature')
-    humidity = st.text_input('Enter value of humidity')
-    ph = st.text_input('Enter value of pH')
-    rainfall = st.text_input('Enter value of rainfall')
+    output=''
+    if st.button('recommended crop'):
+        output=prediction([N,P,K,temp,humidity,ph,rainfall])
     
-    loaded_model = load_model()
-    
-    if loaded_model is not None:
-        if st.button('Recommend Crop'):
-            output = prediction(loaded_model, [nitrogen, phosphorus, potassium, temp, humidity, ph, rainfall])
-            st.success(output)
+    st.success(output) 
 
-if __name__ == '__main__':
+
+if __name__=='__main__':
     main()
+    
+    
+    
